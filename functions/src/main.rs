71ef4b4486fc9eefdf90ad7ec3a2768f846ad217
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-01-25 22:53:08 -0300

    Funções com múltiplos parâmtros
    Funções com múltiplos parâmetros

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     another_function(5);
+
+    print_labeled_measurement(5, 'h');
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
 
 fn another_function(x: i32) {
