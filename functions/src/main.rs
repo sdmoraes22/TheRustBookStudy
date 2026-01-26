d55961b5085d37cdbfe3ac4d1964a8f60959f771
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-01-25 23:21:52 -0300

    Estudo de funções com retorno
    Estudo de funções com retorno

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,14 +1,29 @@
 fn main() {
-    //another_function(5);
+    // another_function(5);
 
-    //print_labeled_measurement(5, 'h');
+    // print_labeled_measurement(5, 'h');
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    //    let y = {
+    //        let x = 3;
+    //        x + 1
+    //    };
 
-    println!("The value of y is {y}");
+    //  println!("The value of y is {y}");
+    // let x = five();
+
+    //    println!("The value o x is: {x}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn five() -> i32 {
+    5
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
