17bd237e45fee6c1cfeb7db12c7b6e2546cf8130
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-01-25 23:03:08 -0300

    Expressões dentro de uma função
    Expressões dentro de uma função

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    another_function(5);
+    //another_function(5);
 
-    print_labeled_measurement(5, 'h');
+    //print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is {y}");
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
