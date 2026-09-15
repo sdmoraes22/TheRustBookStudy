e05ffac48079b81a6066a64c42915f18dd803f16
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-14 21:46:23 -0300

    Preparação do estudo sobre referencia e borrowing

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
