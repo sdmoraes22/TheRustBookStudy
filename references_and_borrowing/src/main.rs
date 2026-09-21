e4287a020a313f6f10f7ca75728da89d3cf4ad19
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-20 22:10:58 -0300

    Reference and borrowing study

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -1,9 +1,24 @@
 fn main() {
-    let s1 = String::from("hello");
+    // let s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+    // let len = calculate_length(&s1);
 
-    println!("The length of '{s1}' is {len}.");
+    // println!("The length of '{s1}' is {len}.");
+
+    // let mut s = String::from("Hello");
+    // change(&mut s);
+    // println!("{s}");
+
+    let mut s = String::from("Hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{r1}, {r2}");
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 fn calculate_length(s: &String) -> usize {
