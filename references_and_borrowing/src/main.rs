e3bfdb0e4dcfe224b0a7a12c1290c090d7ea4d1d
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-20 22:33:41 -0300

    Fechamento de estudo de refeência e borrowing

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -11,10 +11,13 @@ fn main() {
 
     let mut s = String::from("Hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    let r1 = &s;
+    let r2 = &s;
 
     println!("{r1}, {r2}");
+
+    let r3 = &mut s;
+    println!("{r3}");
 }
 
 fn change(some_string: &mut String) {
