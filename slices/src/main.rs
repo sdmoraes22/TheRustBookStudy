410fd885d43c605051824525a07c928b91bb0a0c
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-22 20:28:43 -0300

    Adição de exemplos de slices para compreensão da sintaxe

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,19 +1,44 @@
 fn main() {
-    let mut s = String::from("hello world");
+    // let mut s = String::from("hello world");
+    //
+    // let word = first_word(&s);
+    //
+    // s.clear();
+    //
+    // println!("The first word is {word}");
 
-    let word = first_word(&s);
+    let my_string = String::from("hello world");
 
-    s.clear();
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+    let word = first_word(&my_string_literal);
 }
 
-fn first_word(s: &String) -> usize {
+fn second_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if (item == b' ') {
-            return i;
+            return &s[i..];
+        }
+    }
+    return &s[..];
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if (item == b' ') {
+            return &s[0..i];
         }
     }
 
-    s.len()
+    return &s[..];
 }
