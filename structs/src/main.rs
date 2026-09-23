97f6dd7b8eba5f7f9bff76b9be32936c6b0d4f62
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 18:09:25 -0300

    Criação da primeira struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someruseremail@example.com"),
+        sign_in_count: 1,
+    };
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
