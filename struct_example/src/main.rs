8287da222f739c2fb40c92695763351444457333
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 20:59:21 -0300

    Adicionando método com mais parâmetros

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -4,15 +4,33 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let scale = 2;
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    // dbg!(&rect1);
 
     // print!("rect1 is {rect1:?}");
 
@@ -20,8 +38,16 @@ fn main() {
     //     "The area of the rectangle is {} square pixels",
     //     area(&rect1)
     // );
+
+    // println!("The area of rectangle is {} square pixels", rect1.area());
+
+    // println!("The rectangle has a non zero width; it is {}", rect1.width);
+    //
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
