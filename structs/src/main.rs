b85b80386c56c915042fec621c871dfa116f1920
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 18:33:43 -0300

    Declaração de struct em tupla

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,20 +1,31 @@
 fn main() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someruseremail@example.com"),
-        sign_in_count: 1,
-    };
+    // let mut user1 = User {
+    //     active: true,
+    //     username: String::from("someusername123"),
+    //     email: String::from("someruseremail@example.com"),
+    //     sign_in_count: 1,
+    // };
+    //
+    // user1.email = String::from("anotheruseremail@example.com");
+    //
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("anotherexemple@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+    //
+    // let user3 = User {
+    //     email: String::from("structupdatesyntax@example.com"),
+    //     ..user2
+    // };
 
-    user1.email = String::from("anotheruseremail@example.com");
-
-    let user2 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("anotherexemple@example.com"),
-        sign_in_count: user1.sign_in_count,
-    };
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 struct User {
     active: bool,
