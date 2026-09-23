ccb89f0131b9b3a5207c3e08cad4a844532e0389
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 20:27:10 -0300

    Refatoração para struct

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,12 +1,20 @@
+struct Rectange {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let dimensions = (30, 50);
+    let rect1 = Rectange {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(dimensions)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectange) -> u32 {
+    rectangle.width * rectangle.height
 }
