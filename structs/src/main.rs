ddc6c67da6c45200a5b52ad6634aaaa6b901e756
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 18:14:18 -0300

    criação de função para criação de struct de usuário

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -15,3 +15,12 @@ struct User {
     email: String,
     sign_in_count: u64,
 }
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
