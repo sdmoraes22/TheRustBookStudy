870676b109b7a61a3f6e5379041801867144e14f
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 20:37:37 -0300

    Utilizado dbg!() para validar uma expressão

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,23 +1,27 @@
 #[derive(Debug)]
-struct Rectange {
+struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let rect1 = Rectange {
-        width: 30,
+    let scale = 2;
+
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    print!("rect1 is {rect1:?}");
+    dbg!(&rect1);
 
-    println!(
-        "The area of the rectangle is {} square pixels",
-        area(&rect1)
-    );
+    // print!("rect1 is {rect1:?}");
+
+    // println!(
+    //     "The area of the rectangle is {} square pixels",
+    //     area(&rect1)
+    // );
 }
 
-fn area(rectangle: &Rectange) -> u32 {
+fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
