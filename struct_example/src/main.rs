9cdc72cb625af66485f4cb4fccfcc497d458935b
Cristiano Moraes <cmoraes.ti@gmail.com>
2026-09-23 21:10:49 -0300

    Implementação de sintaxe de múltiplos blocos

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -9,15 +9,17 @@ impl Rectangle {
         self.width * self.height
     }
 
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
     fn square(size: u32) -> Self {
         Self {
             width: size,
             height: size,
         }
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
